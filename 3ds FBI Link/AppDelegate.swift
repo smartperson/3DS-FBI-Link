3fdc63bad4b36ff16556e2ed7953103e0b85d815
Varun Mehta <varun@mehtasw.com>
2017-01-20 12:37:17 -0500

    Initial Commit

---
+++ 3ds FBI Link/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  3ds FBI Link
+//
+//  Created by Varun Mehta on 1/20/17.
+//  Copyright © 2017 Varun Mehta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 3ds FBI Link/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  3ds FBI Link
+//
+//  Created by Varun Mehta on 1/20/17.
+//  Copyright © 2017 Varun Mehta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
